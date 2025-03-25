5c1415d41f403a45639a110ff844298b9a911f94
Marco <marcominisgallo@yahoo.it>
2025-03-25 14:09:35 +0100

    homeP ok

--- script/homePage.js
+++ script/homePage.js
@@ -13,8 +13,13 @@ const getSong = function (query) {
     .then((data) => {
       console.log("DATA", data);
       const id = document.getElementById(query);
-      id.innerText = query;
-      id.addEventListener("click", function () {});
+      id.innerHTML = `
+      <a href="albumPage.html?query=${query}" class="text-decoration-none text-white h5">${query}</a>
+      <p class='text-white'>Ascolta il nuovo album di ${query}</p>
+      `;
+      const img = document.querySelector(`img.${query}`);
+      img.setAttribute("src", data.data[2].album.cover);
+      console.log(img);
     })
     .catch((error) => {
       console.log("ERRORE NELLA FETCH", error);
@@ -27,9 +32,9 @@ getSong("Eminem");
 getSong("Olly");
 getSong("Giorgia");
 getSong("Rihanna");
-getSong("Tony Effe");
+getSong("Salmo");
 getSong("Inna");
 getSong("Madonna");
-getSong("Post Malone");
-getSong("David Guetta");
+getSong("Alpha");
+getSong("Shiva");
 getSong("Sia");
