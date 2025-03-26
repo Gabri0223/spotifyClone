14e169b0e4dae4a407b423800e96008509b10fc9
Nyhm13 <ryo131995@gmail.com>
2025-03-26 11:55:48 +0100

    adds player on all pages and new nav bar with search

--- script/artist.js
+++ script/artist.js
@@ -90,3 +90,20 @@ const getArtistDetails = function () {
 };
 
 getArtistDetails();
+
+// bottone cerca desktop
+const spanCerca = document.getElementById("spanCerca");
+const searchIcon = document.getElementById("searchIcon");
+const searchInputWrapper = document.getElementById("searchInputWrapper");
+searchIcon.addEventListener("click", function () {
+  searchInputWrapper.classList.toggle("d-none");
+  spanCerca.classList.toggle("d-none");
+});
+// bottone cerca mobile
+const spanCercaMobile = document.getElementById("spanCercaMobile");
+const searchIconMobile = document.getElementById("searchIconMobile");
+const searchInputWrapperMobile = document.getElementById("searchInputWrapperMobile");
+searchIconMobile.addEventListener("click", function () {
+  searchInputWrapperMobile.classList.toggle("d-none");
+  spanCercaMobile.classList.toggle("d-none");
+});
--- script/homePage.js
+++ script/homePage.js
@@ -131,3 +131,20 @@ getSong("Madonna");
 getSong("Alpha");
 getSong("Shiva");
 getSong("Sia");
+
+// bottone cerca desktop
+const spanCerca = document.getElementById("spanCerca");
+const searchIcon = document.getElementById("searchIcon");
+const searchInputWrapper = document.getElementById("searchInputWrapper");
+searchIcon.addEventListener("click", function () {
+  searchInputWrapper.classList.toggle("d-none");
+  spanCerca.classList.toggle("d-none");
+});
+// bottone cerca mobile
+const spanCercaMobile = document.getElementById("spanCercaMobile");
+const searchIconMobile = document.getElementById("searchIconMobile");
+const searchInputWrapperMobile = document.getElementById("searchInputWrapperMobile");
+searchIconMobile.addEventListener("click", function () {
+  searchInputWrapperMobile.classList.toggle("d-none");
+  spanCercaMobile.classList.toggle("d-none");
+});
